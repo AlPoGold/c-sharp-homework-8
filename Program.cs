27a1b1a08432c6e558fd9132abfb059b78835e73
Polina Kolesnikova <polina@example.com>
2023-03-22 12:12:32 +0300

    adding 60 exercise, need check for line

--- Program.cs
+++ Program.cs
@@ -167,78 +167,78 @@
 // 18 20
 // 15 18
 
-int[,] ReturnMatrix(int rows, int columns)
-{
-    var rand = new Random();
-    int[,] myArray = new int[rows, columns];
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            myArray[i, j] = rand.Next(1, 10);
+// int[,] ReturnMatrix(int rows, int columns)
+// {
+//     var rand = new Random();
+//     int[,] myArray = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             myArray[i, j] = rand.Next(1, 10);
 
-        }
+//         }
 
-    }
-    return myArray;
-}
+//     }
+//     return myArray;
+// }
 
-void PrintMatrix(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-Console.WriteLine("Remember, columns of first matrix MUST BE equal rows of second matrix");
-Console.Write("Please, enter quantity of rows and columns with space between: ");
-string[] line1 = Console.ReadLine()!.Split(" ");
-int rows1 = int.Parse(line1[0]);
-int columns1 = int.Parse(line1[1]);
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// Console.WriteLine("Remember, columns of first matrix MUST BE equal rows of second matrix");
+// Console.Write("Please, enter quantity of rows and columns with space between: ");
+// string[] line1 = Console.ReadLine()!.Split(" ");
+// int rows1 = int.Parse(line1[0]);
+// int columns1 = int.Parse(line1[1]);
 
-Console.Write("Please, enter min and max with space between: ");
-string[] line2 = Console.ReadLine()!.Split(" ");
-int rows2 = int.Parse(line2[0]);
-int columns2 = int.Parse(line2[1]);
+// Console.Write("Please, enter quantity of rows and columns with space between: ");
+// string[] line2 = Console.ReadLine()!.Split(" ");
+// int rows2 = int.Parse(line2[0]);
+// int columns2 = int.Parse(line2[1]);
 
-int[,] matrix1 = ReturnMatrix(rows1, columns1);
-int[,] matrix2 = ReturnMatrix(rows2, columns2);
-Console.WriteLine("Your first matrix");
-PrintMatrix(matrix1);
-Console.WriteLine();
-Console.WriteLine("Your second matrix");
-PrintMatrix(matrix2);
+// int[,] matrix1 = ReturnMatrix(rows1, columns1);
+// int[,] matrix2 = ReturnMatrix(rows2, columns2);
+// Console.WriteLine("Your first matrix");
+// PrintMatrix(matrix1);
+// Console.WriteLine();
+// Console.WriteLine("Your second matrix");
+// PrintMatrix(matrix2);
 
-int[,] MultiplyMatrix(int[,] matrix1, int[,] matrix2)
-{
-    int[,] matrix3 = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
-    for(int i = 0; i < matrix3.GetLength(0); i++)
-    {
-        for(int j = 0; j < matrix3.GetLength(1); j++)
-        {
-            matrix3[i, j] = 0;
-            for(int m = 0; m<matrix1.GetLength(1); m++)
-            {
-                matrix3[i, j]+= matrix1[i, m] * matrix2[m, j];
-            }
-        }
-    }
-    return matrix3;
-}
+// int[,] MultiplyMatrix(int[,] matrix1, int[,] matrix2)
+// {
+//     int[,] matrix3 = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+//     for(int i = 0; i < matrix3.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matrix3.GetLength(1); j++)
+//         {
+//             matrix3[i, j] = 0;
+//             for(int m = 0; m<matrix1.GetLength(1); m++)
+//             {
+//                 matrix3[i, j]+= matrix1[i, m] * matrix2[m, j];//collect value
+//             }
+//         }
+//     }
+//     return matrix3;
+// }
 
 
-if (matrix1.GetLength(1) == matrix2.GetLength(0))
-{
-    int[,] matrix3 = MultiplyMatrix(matrix1, matrix2);
-    Console.WriteLine();
-    Console.WriteLine("Result:");
-    PrintMatrix(matrix3);
-}
-else Console.WriteLine("Please, check sizes of your matrixes!!! Operation is impossible.");
+// if (matrix1.GetLength(1) == matrix2.GetLength(0))
+// {
+//     int[,] matrix3 = MultiplyMatrix(matrix1, matrix2);
+//     Console.WriteLine();
+//     Console.WriteLine("Result:");
+//     PrintMatrix(matrix3);
+// }
+// else Console.WriteLine("Please, check sizes of your matrixes!!! Operation is impossible.");
 
 // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
@@ -246,6 +246,67 @@ else Console.WriteLine("Please, check sizes of your matrixes!!! Operation is imp
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+
+void PrintMatrix(int[, ,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.WriteLine($"{array[i, j, k]}({i}, {j},  {k})");
+                
+
+            }
+            
+
+        }
+        
+    }
+
+}
+
+int[, , ] ReturnMatrix(int rows, int columns, int koef )
+{
+    var rand = new Random();
+    int[, , ] myArray = new int[rows, columns, koef];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = 0; k < koef; k++)
+            {
+                myArray[i, j, k] = rand.Next(10, 100);
+                int i2;
+                int j2;
+                int k2;
+                for (i2 = 0, j2 = 0, k2 = 0; i2 < myArray.GetLength(0) && j2 < myArray.GetLength(1) && k2 < myArray.GetLength(2); i2++, j2++, k2++)
+                {
+                    if (myArray[i, j, k] == myArray[i2, j2, k2])
+                    {
+                        myArray[i, j, k] = rand.Next(10, 100);
+                        break;
+                    }
+                }
+            }
+
+        }
+
+    }
+
+
+    return myArray;
+}
+
+Console.Write("Please, enter sizes your 3D-matrix with spaces between: ");
+string[] line1 = Console.ReadLine()!.Split(" ");
+int rows = int.Parse(line1[0]);
+int columns = int.Parse(line1[1]);
+int koef = int.Parse(line1[2]);
+int[,,] my3Dmatrix = ReturnMatrix(rows, columns, koef);
+PrintMatrix(my3Dmatrix);
+
 // ДОП
 
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
